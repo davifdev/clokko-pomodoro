ea08a3c51dcdf008d41d1de521291a967e708963
Davi Fernandes <davi.fer159@gmail.com>
2025-12-15 18:44:09 -0300

    chore: create pages

--- src/routes/main-router.tsx
+++ src/routes/main-router.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from '../App';
+import History from '../pages/history/history-page';
+import Home from '../pages/home/home-page';
+import About from '../pages/about/about-page';
+import NotFound from '../pages/not-found/not-found-page';
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
