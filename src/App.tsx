7fc206317ec92c7ebc29c717cbc1183f7f226c17
Davi Fernandes <davi.fer159@gmail.com>
2025-12-15 18:33:34 -0300

    chore: add husky setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <h2 className="text-2xl text-red-400 font-semibold">Hello World!</h2>;
+  return (
+    <h2 className="border-2 border-amber-300 p-2 text-2xl font-semibold text-red-400">
+      Hello World!
+    </h2>
+  );
 };
 
 export default App;
